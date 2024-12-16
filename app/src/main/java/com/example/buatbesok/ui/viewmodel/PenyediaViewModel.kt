5f4eaaad6ca610fd947a466103ffcbc0ac92cb5e
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 19:25:32 +0700

    menambhkan DetailMhsViewmodel dan update MhsViewModel

--- app/src/main/java/com/example/buatbesok/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/buatbesok/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.buatbesok.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -16,6 +17,18 @@ object PenyediaViewModel{
         }
         initializer {
             HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+                )
+             }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
