313861eacc6ca3bdbc5da750c2e9ba765f143ba1
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 11:00:45 +0700

    membuat metod update untuk memperbarui informasi mahasiswa

--- app/src/main/java/com/example/buatbesok/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/buatbesok/data/dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.buatbesok.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -20,4 +21,7 @@ interface MahasiswaDao {
 
     @Delete
     suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
