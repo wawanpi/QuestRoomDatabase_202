e7aeb5523445ab9a037cd80f69bb5a69a9f4b211
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 10:28:22 +0700

    membuat class HomeMhsViewModel dengan membuat parameter repositoryMhs

--- app/src/main/java/com/example/buatbesok/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/buatbesok/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.buatbesok.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.buatbesok.data.entity.Mahasiswa
+import com.example.buatbesok.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
