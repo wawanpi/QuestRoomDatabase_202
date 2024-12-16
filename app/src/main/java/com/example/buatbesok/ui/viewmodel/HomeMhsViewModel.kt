65e3439cb97e0b0fe12d9be1199f03e5a1421792
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 10:37:19 +0700

    Variabel homeUiState dan memanggil fungsirepositoryMhs.getAllMhs()

--- app/src/main/java/com/example/buatbesok/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/buatbesok/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,29 @@
 package com.example.buatbesok.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.buatbesok.data.entity.Mahasiswa
 import com.example.buatbesok.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.internal.NopCollector.emit
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
-)
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNOtNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
