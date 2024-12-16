8cb8db73e47b39ddc2578a77c747f7b4880d75dc
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 20:45:39 +0700

    membuat fungsi DetailMhsViewModel

--- app/src/main/java/com/example/buatbesok/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/buatbesok/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,2 +1,64 @@
 package com.example.buatbesok.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.buatbesok.ui.costumwidget.CustomTopAppBar
+import com.example.buatbesok.ui.viewmodel.DetailMhsViewModel
+import com.example.buatbesok.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun DetailMhsViewModel(
+    modifier: Modifier,
+    ViewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: (String) -> Unit = { },
+){
+    Scaffold (
+        topBar = {
+            CustomTopAppBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(ViewModel.detailUiState.value.detailUiEvent.nim)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+    ){
+        innerPadding ->
+        val detailUiState by ViewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                ViewModel.deleteMhs()
+                onDeleteClick
+            }
+        )
+    }
+}
