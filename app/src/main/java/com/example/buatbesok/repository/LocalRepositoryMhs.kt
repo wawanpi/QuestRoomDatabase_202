72ef619980ec46b96d519e62b075cfb5c5f7c96f
wawanpi <muhaguskurniawan@gmail.com>
2024-12-16 11:19:36 +0700

    membuat Metode getAllMhs, getMhs, deleteMhs, danupdateMhs

--- app/src/main/java/com/example/buatbesok/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/buatbesok/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,28 @@ package com.example.buatbesok.repository
 
 import com.example.buatbesok.data.dao.MahasiswaDao
 import com.example.buatbesok.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ): RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
